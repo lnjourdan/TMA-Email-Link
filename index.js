b06bf34e7cfcb6d180888f30fa5eac775a05146e
Loic Jourdan <111344698+lnjourdan@users.noreply.github.com>
2025-02-12 17:09:26 +0000

    Update and rename index.html to index.js
    changed the code from basic html to more user friendly javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+function sendEmail() {
+    const recipient = 'loic.jourdan@tma-consultants.co.uk';
+    const subject = 'TMA BIM Query';
+    
+    const bodyText = `
+Hi,
+
+This question is related to the department: Landscape / Arb Risk / Arb Planning / Ecology / Technical / Other (Remove those not relevant)
+This question is in relation to the project: XXXXXX (Please add the project number here if relevant)
+
+My Query:
+
+
+Kind Regards,
+
+(Make sure to add your name in here!)
+    `.trim(); // Trim removes any leading/trailing empty lines
+
+    const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(bodyText)}`;
+    window.location.href = mailtoLink;
+}
